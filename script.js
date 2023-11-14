6b44f95481ee48b6cf0483150a384880ce9fa497
Anatolii <ahryhorzhevskyi@myseneca.ca>
2023-11-14 00:37:06 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,47 @@
+const dropdownButton = document.getElementById('myDropdownButton');
+const dropdownMenu = document.getElementById('myDropdownMenu');
+if (dropdownButton) {
+dropdownButton.addEventListener('click', () => {
+  dropdownMenu.classList.toggle('show');
+});
+}
+
+const dropdownButton1 = document.getElementById('myDropdownButton1');
+const dropdownMenu1 = document.getElementById('myDropdownMenu1');
+if (dropdownButton1) {
+dropdownButton1.addEventListener('click', () => {
+  dropdownMenu1.classList.toggle('show');
+});
+}
+
+const dropdownButton2 = document.getElementById('myDropdownButton2');
+const dropdownMenu2 = document.getElementById('myDropdownMenu2');
+if (dropdownButton2) {
+dropdownButton2.addEventListener('click', () => {
+  dropdownMenu2.classList.toggle('show');
+});
+}
+
+const dropdownButton3 = document.getElementById('myDropdownButton3');
+const dropdownMenu3 = document.getElementById('myDropdownMenu3');
+if (dropdownButton3) {
+dropdownButton3.addEventListener('click', () => {
+  dropdownMenu3.classList.toggle('show');
+});
+}
+
+const dropdownButton4 = document.getElementById('myDropdownButton4');
+const dropdownMenu4 = document.getElementById('myDropdownMenu4');
+if (dropdownButton4) {
+dropdownButton4.addEventListener('click', () => {
+  dropdownMenu4.classList.toggle('show');
+});
+}
+
+const dropdownButton5 = document.getElementById('myDropdownButton5');
+const dropdownMenu5 = document.getElementById('myDropdownMenu5');
+if (dropdownButton5) {
+dropdownButton5.addEventListener('click', () => {
+  dropdownMenu5.classList.toggle('show');
+});
+}
